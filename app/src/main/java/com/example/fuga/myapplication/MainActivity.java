f22ebd9a42f6007fe3a72d0164f5b6181ca00b36
오태현 <gymnopedy01@gmail.com>
2017-05-01 20:33:34 +0900

    Hello앱 Admob 작업

---
+++ app/src/main/java/com/example/fuga/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+
+package com.example.fuga.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(),  "ca-app-pub-9602338938206560~5694043532");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+    }
+}
